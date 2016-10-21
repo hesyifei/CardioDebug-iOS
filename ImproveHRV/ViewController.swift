c915291836bb1196d7143a03f1fb74a4e897c14f
Yifei He <eflyjason@gmail.com>
2016-10-21 12:10:15 +0800

    add basic code

--- ImproveHRV/ViewController.swift
+++ ImproveHRV/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 import BITalinoBLE
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BITalinoBLEDelegate {
+
+	@IBOutlet var stateLabel: UILabel!
+
+	var bitalino: BITalinoBLE!
+
+	var allArr: [Int]!
+	var sampleRate: Int!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+
+		allArr = []
+		sampleRate = 100
+
+		bitalino = BITalinoBLE.init(uuid: "1AC1F712-C6FE-4728-9BEF-DBD2A6177D47")
+		bitalino.delegate = self
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -21,6 +34,62 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func connectButtonAction(_ sender: AnyObject) {
+		if(!bitalino.isConnected){
+			stateLabel.text = "..."
+			bitalino.scanAndConnect()
+		} else{
+			bitalino.disconnect()
+		}
+	}
 
+	@IBAction func startButtonAction(_ sender: AnyObject) {
+		if(bitalino.isConnected){
+			if(!bitalino.isRecording){
+				let inputs: [Int] = [0, 1, 2, 3, 4, 5]
+
+				bitalino.startRecording(fromAnalogChannels: inputs, withSampleRate: sampleRate, numberOfSamples: 50, samplesCompletion: { (frame: BITalinoFrame?) -> Void in
+					if let result = frame?.a1 {
+						if result != 0 {
+							print("\(result)")
+							self.allArr.append(result)
+						}
+					}
+				})
+
+			} else{
+				bitalino.stopRecording()
+
+				print(allArr.description)
+			}
+		} else{
+			print("NOT CON")
+		}
+	}
+
+
+	func bitalinoDidConnect(_ bitalino: BITalinoBLE!) {
+		stateLabel.text = "YAYA"
+	}
+
+	func bitalinoDidDisconnect(_ bitalino: BITalinoBLE!) {
+		stateLabel.text = "NO"
+	}
+
+	func bitalinoRecordingStarted(_ bitalino: BITalinoBLE!) {
+		print("STARTED")
+	}
+
+	func bitalinoRecordingStopped(_ bitalino: BITalinoBLE!) {
+		print("ENDED")
+	}
+
+	func bitalinoBatteryThresholdUpdated(_ bitalino: BITalinoBLE!) {
+		//do
+	}
+
+	func bitalinoBatteryDigitalOutputsUpdated(_ bitalino: BITalinoBLE!) {
+		//do
+	}
+	
 }
-
