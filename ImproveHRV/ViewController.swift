7fa3f15e73a3d68cc5c6e9dd810dda7c7aeff5d9
Yifei He <eflyjason@gmail.com>
2016-10-21 11:51:54 +0800

    Initial Commit

---
+++ ImproveHRV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImproveHRV
+//
+//  Created by Jason Ho on 21/10/2016.
+//  Copyright © 2016 Arefly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
