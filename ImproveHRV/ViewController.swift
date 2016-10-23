afda07a032458c3a8bd80e58fce9753e773e728b
Yifei He <eflyjason@gmail.com>
2016-10-23 12:18:25 +0800

    [will not working as the SDK only update data once 0.5s] try to show live heart rate

--- ImproveHRV/ViewController.swift
+++ ImproveHRV/ViewController.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import Async
 import BITalinoBLE
+import Charts
 
 class ViewController: UIViewController, BITalinoBLEDelegate {
 
 	@IBOutlet var mainLabel: UILabel!
 	@IBOutlet var mainButton: UIButton!
+	@IBOutlet var mainChart: LineChartView!
 
 	var bitalino: BITalinoBLE!
 
 	var timer: Timer!
+	var startTime: Date!
 	var endTime: Date!
+
+	var chartTimer: Timer!
+	var lastUpdateTime: Date!
 
 	var rawData: [Int]!
 
@@ -46,6 +53,25 @@ class ViewController: UIViewController, BITalinoBLEDelegate {
 		mainButton.addTarget(self, action: #selector(self.mainButtonAction), for: .touchUpInside)
 
 		mainLabel.text = "Unconnected"
+
+
+		var dataEntries: [ChartDataEntry] = []
+		let values = [0]
+		for (index, value) in values.enumerated() {
+			let dataEntry = ChartDataEntry(x: Double(index), y: Double(value))
+			dataEntries.append(dataEntry)
+		}
+		let set_a: LineChartDataSet = LineChartDataSet(values: dataEntries, label: "a")
+		set_a.drawCirclesEnabled = false
+		set_a.setColor(UIColor.blue)
+
+		self.mainChart.data = LineChartData(dataSet: set_a)
+
+		self.mainChart.data?.addEntry(ChartDataEntry(x: Double(1), y: Double(5)), dataSetIndex: 0)
+		//self.mainChart.data?.addXValue(String(i))
+		self.mainChart.setVisibleXRange(minXRange: Double(CGFloat(1)), maxXRange: Double(CGFloat(50)))
+		self.mainChart.notifyDataSetChanged()
+		self.mainChart.moveViewToX(Double(CGFloat(1)))
 	}
 
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -78,8 +104,14 @@ class ViewController: UIViewController, BITalinoBLEDelegate {
 				//self.mainLabel.text = "Recording..."
 
 				timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timerAction), userInfo: nil, repeats: true)
-				endTime = Date.init().addingTimeInterval(20)
+				startTime = Date.init()
+				endTime = startTime.addingTimeInterval(20)
 				self.timerAction()		// 為避免延遲一秒才開始執行
+
+
+				chartTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.chartTimerAction), userInfo: nil, repeats: true)
+				lastUpdateTime = Date.init()
+
 
 				bitalino.startRecording(fromAnalogChannels: [0, 1, 2, 3, 4, 5], withSampleRate: 100, numberOfSamples: 50, samplesCompletion: { (frame: BITalinoFrame?) -> Void in
 					if let result = frame?.a1 {
@@ -96,23 +128,19 @@ class ViewController: UIViewController, BITalinoBLEDelegate {
 	}
 
 	func stopRecording() {
-		if rawData.count > 10*100 {
-			bitalino.stopRecording()
+		bitalino.stopRecording()
 
-			self.mainLabel.text = "Finished"
-			mainButton.isEnabled = true
+		self.mainLabel.text = "Finished"
+		mainButton.isEnabled = true
 
-			print(rawData.description)
+		print(rawData.description)
 
-			self.performSegue(withIdentifier: ResultViewController.SHOW_RESULT_SEGUE_ID, sender: self)
-		} else {
-			print("MUST > 10 SEC")
-		}
+		self.performSegue(withIdentifier: ResultViewController.SHOW_RESULT_SEGUE_ID, sender: self)
 	}
 
 	func timerAction() {
 		let timeTill = endTime.timeIntervalSinceNow
-		print(timeTill)
+		//print(timeTill)
 		if timeTill <= 0 {
 			mainLabel.text = "00:00"
 			if let _ = timer {
@@ -127,6 +155,45 @@ class ViewController: UIViewController, BITalinoBLEDelegate {
 			} else {
 				mainLabel.text = String(format: "%02d:%02d", m, s)
 			}
+		}
+	}
+
+	func chartTimerAction() {
+		let timeFrom = abs(startTime.timeIntervalSinceNow)
+		print(timeFrom)
+		let timeTill = endTime.timeIntervalSinceNow
+		if timeTill <= 0 {
+			if let _ = chartTimer {
+				chartTimer?.invalidate()
+				chartTimer = nil
+			}
+		} else {
+			let timeBetween = Date.init().timeIntervalSince(lastUpdateTime)
+			print(timeBetween)
+			print(Int(timeBetween*100))
+			print(Int(timeFrom*100))
+
+			let startFrame = Int(timeFrom*100)
+			let endFrame = startFrame+Int(timeBetween*100)-2
+
+			print(rawData.count)
+			/*for index in startFrame...endFrame {
+				print("**")
+				print(rawData[index])
+			}*/
+
+			lastUpdateTime = Date.init()
+			/*if rawData.count-1 >= self.frameCount {
+				Async.main {
+					self.mainChart.data?.addEntry(ChartDataEntry(x: Double(self.frameCount), y: Double(5)), dataSetIndex: 0)
+					//self.mainChart.data?.addXValue(String(i))
+					self.mainChart.setVisibleXRange(minXRange: Double(CGFloat(1)), maxXRange: Double(CGFloat(50)))
+					self.mainChart.notifyDataSetChanged()
+					self.mainChart.moveViewToX(Double(CGFloat(self.frameCount)))
+				}
+			}*/
+
+
 		}
 	}
 
