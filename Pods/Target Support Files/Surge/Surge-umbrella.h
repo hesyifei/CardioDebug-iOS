9219626c340c1719ea6b2fd13d9060e076ad3948
Yifei He <eflyjason@gmail.com>
2016-10-29 11:02:02 +0800

    add pod Surge

--- Pods/Target Support Files/Surge/Surge-umbrella.h
+++ Pods/Target Support Files/Surge/Surge-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double ChartsVersionNumber;
-FOUNDATION_EXPORT const unsigned char ChartsVersionString[];
+FOUNDATION_EXPORT double SurgeVersionNumber;
+FOUNDATION_EXPORT const unsigned char SurgeVersionString[];
 
