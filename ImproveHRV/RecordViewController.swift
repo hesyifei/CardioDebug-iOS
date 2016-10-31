4f45a2a72dd7628b7e052c5ac17ef0b9c56ded22
Yifei He <eflyjason@gmail.com>
2016-10-31 22:51:13 +0800

    add basic code for displaying record & going into detail view

--- ImproveHRV/RecordViewController.swift
+++ ImproveHRV/RecordViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 import Foundation
+import RealmSwift
 
-class RecordViewController: UIViewController {
+class RecordViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+	@IBOutlet var tableView: UITableView!
+
+	var realm: Realm!
+
+	var tableData: [ECGData]!
 
 	// MARK: - override func
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		tableView.delegate = self
+		tableView.dataSource = self
+
+
+		realm = try! Realm()
+
+		tableData = []
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
+		print("RecordViewController viewWillAppear")
 
+		let allECGData = realm.objects(ECGData.self)
+		print(allECGData)
+
+		tableData = Array(allECGData)
+
+		self.tableView.reloadSections(NSIndexSet(index: 0) as IndexSet, with: .automatic)
 	}
 
 	override func viewDidDisappear(_ animated: Bool) {
@@ -27,8 +48,45 @@ class RecordViewController: UIViewController {
 
 	}
 
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == ResultViewController.SHOW_RESULT_SEGUE_ID {
+			if let destination = segue.destination as? ResultViewController {
+				if let indexPath: IndexPath = self.tableView.indexPathForSelectedRow {
+					let data = tableData[indexPath.row]
+
+					let passedData = PassECGResult()
+					passedData.startDate = data.startDate
+					passedData.rawData = data.rawData
+					passedData.isNew = false
+
+					destination.passedData = passedData
+
+
+					self.tableView.deselectRow(at: indexPath, animated: true)
+				}
+			}
+		}
+	}
+
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+
+
+	// MARK: - tableView related
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return tableData.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+		cell.textLabel?.text = "\(tableData[indexPath.row].duration)"
+		cell.detailTextLabel?.text = "\(tableData[indexPath.row].startDate)"
+		return cell
+	}
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		self.performSegue(withIdentifier: ResultViewController.SHOW_RESULT_SEGUE_ID, sender: self)
 	}
 }
