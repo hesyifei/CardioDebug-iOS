eaaa9584566a0d15f1110c4bfc91ffe1a1256571
Yifei He <eflyjason@gmail.com>
2016-10-31 22:16:45 +0800

    add basic RecordViewController use tab bar view controller

---
+++ ImproveHRV/RecordViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RecordViewController.swift
+//  ImproveHRV
+//
+//  Created by Jason Ho on 31/10/2016.
+//  Copyright © 2016 Arefly. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class RecordViewController: UIViewController {
+
+	// MARK: - override func
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+	}
+
+	override func viewDidDisappear(_ animated: Bool) {
+		super.viewDidDisappear(animated)
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+}
