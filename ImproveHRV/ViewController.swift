d270a99402a43258d7333e87243fd096af2320f1
Yifei He <eflyjason@gmail.com>
2016-11-02 23:17:35 +0800

    add tableView in ViewController

--- ImproveHRV/ViewController.swift
+++ ImproveHRV/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+	@IBOutlet var tableView: UITableView!
+
+	var tableData = [String]()
 
 	// MARK: - override func
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		tableView.delegate = self
+		tableView.dataSource = self
+
+		tableData = ["Run", "Swim", "Jump"]
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -30,5 +38,25 @@ class ViewController: UIViewController {
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+
+
+	// MARK: - tableView related
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return tableData.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+		let result = tableData[indexPath.row]
+		cell.textLabel?.text = "\(result)"
+		return cell
+	}
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let cell = self.tableView.cellForRow(at: indexPath)
+		cell?.accessoryType = .checkmark
+		cell?.isUserInteractionEnabled = false
+		self.tableView.deselectRow(at: indexPath, animated: true)
 	}
 }
