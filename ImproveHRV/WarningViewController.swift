406627dc5a964f77628e823112793a53c601534c
Yifei He <eflyjason@gmail.com>
2016-12-18 22:54:36 +0800

    add basic code to WarningViewController

--- ImproveHRV/WarningViewController.swift
+++ ImproveHRV/WarningViewController.swift
@@ -25,15 +25,22 @@ class WarningViewController: UIViewController {
 	@IBOutlet var rightButton: UIButton!
 
 	// MARK: - init var
+	var currentState = 0
 
 	// MARK: - data var
-
-
+	var symptoms = [String]()
 
 	// MARK: - override func
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		symptoms = ["Headache", "Heart attack"]
+
+		leftButton.isHidden = true
+		leftButton.addTarget(self, action: #selector(self.leftButtonAction), for: .touchUpInside)
+
+		rightButton.setTitle("Next", for: .normal)
+		rightButton.addTarget(self, action: #selector(self.rightButtonAction), for: .touchUpInside)
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -49,6 +56,41 @@ class WarningViewController: UIViewController {
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+
+	func leftButtonAction() {
+		if currentState > 0 {
+			appendState()
+		}
+	}
+
+	func rightButtonAction() {
+		appendState()
+	}
+
+	func appendState() {
+		switch currentState {
+		case 0:
+			leftButton.setTitle("Yes", for: .normal)
+			leftButton.isHidden = false
+			rightButton.setTitle("No", for: .normal)
+			mainLabel.text = "Did you feel..."
+			mainTextView.text = "\(symptoms[currentState])?"
+			break
+		case symptoms.count:
+			leftButton.isHidden = true
+			rightButton.setTitle("Close", for: .normal)
+			mainLabel.text = "Recommendation:"
+			mainTextView.text = "GO HOSPITAL NOW"
+			break
+		case symptoms.count+1:
+			self.dismiss(animated: true, completion: nil)
+			break
+		default:
+			mainTextView.text = "\(symptoms[currentState])?"
+			break
+		}
+		currentState += 1
 	}
 	
 }
