87cfbc7ff7f41c5c8c54640821434fd559443bd4
Yifei He <eflyjason@gmail.com>
2017-01-14 00:04:37 +0800

    add basic code for a "Add" button in the center of the tab bar update tab bar icon

---
+++ ImproveHRV/CustomTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  CustomTabBarController.swift
+//  ImproveHRV
+//
+//  Created by Arefly on 1/13/17.
+//  Copyright © 2017 Arefly. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
+	override func viewDidLoad() {
+		self.delegate = self
+	}
+	/*
+	// http://stackoverflow.com/a/29011197/2603230
+	override func viewWillLayoutSubviews() {
+	var tabFrame = self.tabBar.frame
+	// - 45 is editable , the default value is 49 px, below lowers the tabbar and above increases the tab bar size
+	tabFrame.size.height = 45
+	tabFrame.origin.y = self.view.frame.size.height - 45
+	self.tabBar.frame = tabFrame
+	}*/
+	func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+		if viewController.restorationIdentifier == "NewButtonHolderView" {
+			print("selected NewButtonHolderView")
+			return false
+		}
+		return true
+	}
+}
