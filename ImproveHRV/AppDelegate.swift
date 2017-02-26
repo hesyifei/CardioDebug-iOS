0d278b47f485abda58e7950178bcdbd91c756c53
Yifei He <eflyjason@gmail.com>
2017-02-26 09:55:32 +0800

    add migration for Realm

--- ImproveHRV/AppDelegate.swift
+++ ImproveHRV/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 #if DEBUG
 	import TouchVisualizer
 #endif
@@ -31,6 +32,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 				Visualizer.start()
 			}
 		#endif
+
+		let config = Realm.Configuration(
+			// Set the new schema version. This must be greater than the previously used
+			// version (if you've never set a schema version before, the version is 0).
+			schemaVersion: 1,
+
+			// Set the block which will be called automatically when opening a Realm with
+			// a schema version lower than the one set above
+			migrationBlock: { migration, oldSchemaVersion in
+				// We haven’t migrated anything yet, so oldSchemaVersion == 0
+				if (oldSchemaVersion < 1) {
+					migration.renameProperty(onType: ECGData.className(), from: "_backingRawData", to: "_ecgRawData")
+				}
+		})
+		// Tell Realm to use this new configuration object for the default Realm
+		Realm.Configuration.defaultConfiguration = config
 
 		return true
 	}
